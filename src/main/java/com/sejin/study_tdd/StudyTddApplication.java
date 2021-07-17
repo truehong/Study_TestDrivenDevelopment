ff34696eeb568880ce8c25687983fc202f1a518e
truehong <hhhhhongse@gmail.com>
2021-07-17 12:21:15 +0900

    Initial commit

---
+++ src/main/java/com/sejin/study_tdd/StudyTddApplication.java
@@ -0,0 +1,13 @@
+package com.sejin.study_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sejin/study_tdd/StudyTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sejin.study_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
